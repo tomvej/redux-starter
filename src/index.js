c58bf4af290d4173f880f0ac9f4e077d44c7b9c5
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2016-07-15 16:30:26 +0200

    Redux provider plugged in.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
 import './index.less'
 
 render(
-    <div>Testing page</div>,
+    <Provider store={createStore(state => state, {})}>
+        <div>It works!</div>
+    </Provider>,
     document.getElementById('content')
 );
