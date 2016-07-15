bc462f908d04a9789b6c32af84efe6bcb13edfd9
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2016-07-15 16:20:15 +0200

    Webpack and react

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    entry: './src/',
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/dist/',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: '/node_modules/',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
+            {
+                test: /\.less$/,
+                loader: 'style!css!less'
+            }
+        ]
+    }
+};
