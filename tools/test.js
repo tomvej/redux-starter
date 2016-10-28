be9cdc3a35ca82ae4413046b9989f7008ab92637
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2016-10-28 13:12:29 +0200

    Enzyme and its assertions

--- tools/test.js
+++ tools/test.js
@@ -3,8 +3,10 @@ import chai, {should} from 'chai';
 import chaiImmutable from 'chai-immutable';
 import dirtyChai from 'dirty-chai';
 import sinonChai from 'sinon-chai';
+import chaiEnzyme from 'chai-enzyme'
 
 chai.use(chaiImmutable);
+chai.use(chaiEnzyme());
 chai.use(dirtyChai);
 chai.use(sinonChai);
 
