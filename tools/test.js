5f76270552d3e5497ef3358e711fe1d892c69f87
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2016-10-28 12:57:18 +0200

    Additional test libraries

--- tools/test.js
+++ tools/test.js
@@ -1,4 +1,11 @@
 import 'babel-polyfill';
-import {should} from 'chai';
+import chai, {should} from 'chai';
+import chaiImmutable from 'chai-immutable';
+import dirtyChai from 'dirty-chai';
+import sinonChai from 'sinon-chai';
+
+chai.use(chaiImmutable);
+chai.use(dirtyChai);
+chai.use(sinonChai);
 
 should();
