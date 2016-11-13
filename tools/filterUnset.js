49d379abc4b87274de0cfce8958998af32109929
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2016-11-13 10:14:22 +0100

    Filter unset goes down the object hierarchy

--- tools/filterUnset.js
+++ tools/filterUnset.js
@@ -1,7 +1,11 @@
-export default (config) => Object.keys(config).filter((key) => config[key]).reduce((result, key) => {
+const filterUnset = (config) => Object.keys(config).filter((key) => config[key]).reduce((result, key) => {
     let value = config[key];
     if (Array.isArray(value)) {
         value = value.filter((item) => item);
+    } else if (typeof value === 'object') {
+        value = filterUnset(value);
     }
     return Object.assign({[key]: value}, result);
 }, {});
+
+export default filterUnset;
--- tools/filterUnset.spec.js
+++ tools/filterUnset.spec.js
@@ -1,16 +1,18 @@
 import filterUnset from './filterUnset';
 
 describe('filter Unset', () => {
-    it('leaves empty object as it is', () => {
+    const sparseArray = [undefined, 4, 5, undefined, 2, 3];
+    const denseArray = [4, 5, 2, 3];
+    const primitive = {
+        number: 5,
+        string: 'string',
+        boolean: true,
+    };
+    it('preserves empty object', () => {
         filterUnset({}).should.deep.equal({});
     });
-    it('leaves primitive properties as they are', () => {
-        const target = {
-            number: 5,
-            string: 'string',
-            boolean: true,
-        };
-        filterUnset(target).should.deep.equal(target);
+    it('preserves primitive properties', () => {
+        filterUnset(primitive).should.deep.equal(primitive);
     });
     it('removes undefined properties', () => {
         filterUnset({
@@ -19,9 +21,36 @@ describe('filter Unset', () => {
     });
     it('removes undefined items from array properties', () => {
         filterUnset({
-            array: [undefined, 4, 5, undefined, 2, 3],
+            array: sparseArray,
         }).should.deep.equal({
-            array: [4, 5, 2, 3],
+            array: denseArray,
+        });
+    });
+    it('removes undefined items from array properties deep in object hierarchy', () => {
+        filterUnset({
+            object: {
+                array: sparseArray,
+            },
+        }).should.deep.equal({
+            object: {
+                array: denseArray,
+            },
+        });
+    });
+    it('removes undefined properties deep in object hierarchy', () => {
+        filterUnset({
+            object: {
+                empty: undefined,
+            },
+        }).should.deep.equal({
+            object: {},
+        });
+    });
+    it('preserves primitive properties deep in object hierarchy', () => {
+        filterUnset({
+            object: primitive,
+        }).should.deep.equal({
+            object: primitive,
         });
     });
 });
